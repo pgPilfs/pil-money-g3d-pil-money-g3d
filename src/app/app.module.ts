a28c73955f1568a16aff214a83596d0a28e30dcd
PipiCode <68872811+aylen916@users.noreply.github.com>
2021-06-27 19:59:53 -0300

    Revert "Registro us2"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RegistroComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
