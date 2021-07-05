137a89962d8451b027d58712180df2f6fbcfe120
Fernando Herrera <69762889+1w3112006UtreraAgustin@users.noreply.github.com>
2021-07-04 22:20:11 -0300

    Registro reactivo

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+ FormBuilder,
+
+ FormControl,
+ Validators
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +14,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-
-  constructor() { }
+  registroForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
+  initializeForm(): void {
+    this.registroForm = this.fb.group({
+        name: ['', Validators.required],
+        surname:['',Validators.required],
+        email:new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required]),
+        phoneNumber: ['', Validators.required],
+        phoneNumber2:['', Validators.required],
+        address: ['', Validators.required],
+        address2: ['', Validators.required],
+        city: ['', Validators.required],
+        provincias: ['', Validators.required],
 
+        termsYcond: [false, Validators.required],
+      
+    
+    });
+
+}
+
+onSubmit(): void{
+  console.log(this.registroForm);
+}
 }
