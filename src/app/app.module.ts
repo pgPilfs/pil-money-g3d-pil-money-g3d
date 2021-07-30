1138577b86e85752100058198dd97d8340faa625
Sofía Juarez <sjuarez@simtlix.com>
2021-07-30 17:03:05 -0300

    navbar funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InicioComponent } from './inicio/inicio.component';
 import { MenuComponent } from './menu/menu.component';
 
+import{ScriptServiceService} from './Services/script-service.service';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,9 @@ import { MenuComponent } from './menu/menu.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    ScriptServiceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ScriptServiceService} from './../Services/script-service.service'
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _CargarScript:ScriptServiceService) { 
+    _CargarScript.Carga(["script"]);
+  }
 
   ngOnInit(): void {
   }
